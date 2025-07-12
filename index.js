2c470fe6d12db776f86cd6c136f686c700b1fcc8
Tyler <tyler@keyesfamily.ca>
2025-07-12 16:50:43 -0400

    Fix: Added popular cards command
    Added popular cards command to index.js

--- index.js
+++ index.js
@@ -6,44 +6,54 @@ const { PrismaClient, CardRarity } = require("@prisma/client");
 config({ path: __dirname + "/.env" });
 const prisma = new PrismaClient();
 const bot = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 // ME, NICK, JORGE, PIN
-const ADMIN_IDS = ["224231121720705044", "543652818855133196", "429116919967121418", "363341878642343936"];
+const ADMIN_IDS = [
+  "224231121720705044",
+  "543652818855133196",
+  "429116919967121418",
+  "363341878642343936",
+];
 const tradeSessions = new Map();
 
 bot.once("ready", () => {
-    console.log(`${bot.user.username} is online!`);
+  console.log(`${bot.user.username} is online!`);
 });
 
 const commands = [
-    require("./commands/addcard"),
-    require("./commands/book"),
-    require("./commands/canceltrade"),
-    require("./commands/cardinfo"),
-    require("./commands/cardlist"),
-    require("./commands/completeleaderboard"),
-    require("./commands/completion"),
-    require("./commands/flex"),
-    require("./commands/importcards"),
-    require("./commands/inventory"),
-    require("./commands/leaderboard"),
-    require("./commands/missingcards"),
-    require("./commands/ntchelp"),
-    require("./commands/rarestcards"),
-    require("./commands/spin"),
-    require("./commands/trade")
+  require("./commands/addcard"),
+  require("./commands/book"),
+  require("./commands/canceltrade"),
+  require("./commands/cardinfo"),
+  require("./commands/cardlist"),
+  require("./commands/completeleaderboard"),
+  require("./commands/completion"),
+  require("./commands/flex"),
+  require("./commands/importcards"),
+  require("./commands/inventory"),
+  require("./commands/leaderboard"),
+  require("./commands/missingcards"),
+  require("./commands/ntchelp"),
+  require("./commands/popularcards"),
+  require("./commands/rarestcards"),
+  require("./commands/spin"),
+  require("./commands/trade"),
 ];
 
-bot.on("messageCreate", async message => {
-    if (message.author.bot) return;
-    const content = message.content.trim();
+bot.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  const content = message.content.trim();
 
-    for (const cmd of commands) {
-        if (content.toLowerCase().startsWith(cmd.name)) {
-            return cmd.execute(message, { prisma, bot, ADMIN_IDS, tradeSessions });
-        }
+  for (const cmd of commands) {
+    if (content.toLowerCase().startsWith(cmd.name)) {
+      return cmd.execute(message, { prisma, bot, ADMIN_IDS, tradeSessions });
     }
+  }
 });
 
 bot.login(process.env.TOKEN);
